342faa6252dfad9fc959831db53091464a9c36c0
Forest1sland <1548893870@qq.com>
2022-11-05 15:05:41 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <show_qr></show_qr>
+</template>
+
+<script setup>
+import show_qr from './components/show_qr.vue';
+
+</script>
+
+<script >
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
